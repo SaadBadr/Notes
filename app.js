932e63e49451ca8d795316929e6081fbae6bf0b7
SaadBadr <saadbadr23@gmail.com>
2021-01-29 04:20:00 +0200

    starting user module creating the model of the user

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const morgan = require('morgan');
-const rateLimit = require('express-rate-limit');
-const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
-const hpp = require('hpp');
+const express = require("express");
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 
-const AppError = require('./utils/appError');
-const globalErrorHandler = require('./controllers/errorController');
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 
 const app = express();
 
@@ -16,17 +16,17 @@ const app = express();
 app.use(helmet());
 
 // Development logging
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
-  message: 'Too many requests from this IP, please try again in an hour!',
+  message: "Too many requests from this IP, please try again in an hour!",
 });
-app.use('/api', limiter);
+app.use("/api", limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json());
@@ -51,7 +51,11 @@ app.use(express.static(`${__dirname}/public`));
 
 //ROUTES HERE!!!
 
-app.all('*', (req, res, next) => {
+const userRouter = require("./routes/userRoutes");
+
+app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
---
+++ models/userModel.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "please provide your email!"],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "please provide a valid email!"],
+  },
+  password: {
+    type: String,
+    minlength: [8, "password should be of at least 8 characters!"],
+    required: true,
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(confirm) {
+        return this.password === confirm;
+      },
+      message: "password should matches",
+    },
+  },
+  passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
+});
+
+userSchema.pre("save", async function(next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+});
+
+userSchema.pre("save", function(next) {
+  if (!this.isModified("password") || this.isNew) return next();
+  this.passwordChangedAt = Date.now() - 1000;
+});
+
+userSchema.methods.correctPassword = async function(
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.ifPasswordChangedAfter = function(time) {
+  return (
+    this.passwordChangedAt && this.passwordChangedAt.getTime() / 1000 > time
+  );
+};
+
+userSchema.methods.createPasswordResetToken = function() {
+  const resetToken = crypto.randomBytes(32).toString("hex");
+  this.passwordResetToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000; //time is in msec
+  console.log({ resetToken }, this.passwordResetToken);
+  return resetToken;
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
